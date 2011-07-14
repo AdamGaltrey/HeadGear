7795058607eeaab5f6622e3194c520c34074532e
iPhysX <jasonwalsh@blueyonder.co.uk>
2011-07-14 00:09:25 -0700

    First Commit

---
+++ src/net/minedev/headgear/HeadGear.java
@@ -0,0 +1,17 @@
+package net.minedev.headgear;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HeadGear extends JavaPlugin
+{
+	public void onEnable()
+	{
+		
+	}
+	
+	public void onDisable()
+	{
+		
+	}
+
+}
