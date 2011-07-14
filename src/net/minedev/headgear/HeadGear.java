603b0b1e7c496fa56b098c158502481e9d122551
iPhysX <jasonwalsh@blueyonder.co.uk>
2011-07-14 00:29:54 -0700

    moveevent

--- src/net/minedev/headgear/HeadGear.java
+++ src/net/minedev/headgear/HeadGear.java
@@ -1,12 +1,17 @@
 package net.minedev.headgear;
 
+import net.minedev.listeners.HgPlayerListener;
+
+import org.bukkit.event.Event.Priority;
+import org.bukkit.event.Event.Type;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HeadGear extends JavaPlugin
 {
 	public void onEnable()
 	{
-		
+		setupListeners();
 	}
 	
 	public void onDisable()
@@ -14,14 +19,12 @@ public class HeadGear extends JavaPlugin
 		
 	}
 	
-	public void setupPermissions()
-	{
-		
-	}
-	
 	public void setupListeners()
 	{
-		
+		pm = getServer().getPluginManager();
+		pm.registerEvent(Type.PLAYER_MOVE, playerlistener, Priority.Normal, this);
 	}
 
+	PluginManager pm;
+	public HgPlayerListener playerlistener = new HgPlayerListener();
 }
---
+++ src/net/minedev/listeners/HgPlayerListener.java
@@ -0,0 +1,22 @@
+package net.minedev.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
+
+public class HgPlayerListener extends PlayerListener{
+
+	public void onPlayerMove(PlayerMoveEvent event)
+	{
+
+		Player p = event.getPlayer();
+		if (p.getInventory().getHelmet().equals(57))
+		{
+			Vector dir = p.getLocation().getDirection().multiply(((3)*(0.3))/2).setY(0.1);
+			p.setVelocity(dir);
+		}
+	}
+}
+
+
