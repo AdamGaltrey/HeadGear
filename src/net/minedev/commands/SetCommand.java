d9f8d2f0d110d68a82042db74ba58899d2f89ee9
iPhysX <jasonwalsh@blueyonder.co.uk>
2011-07-14 00:55:29 -0700

    push

--- src/net/minedev/commands/SetCommand.java
+++ src/net/minedev/commands/SetCommand.java
@@ -1,16 +1,45 @@
 package net.minedev.commands;
 
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import couk.Adamki11s.Extras.Player.ExtrasPlayer;
 
 public class SetCommand implements CommandExecutor {
 
+	ExtrasPlayer extrasPlayer = new ExtrasPlayer();
+
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2,
-			String[] arg3) {
-		
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		if (cmd.getName().equalsIgnoreCase("set"))
+		{
+
+			extrasPlayer.setBlockOnPlayerHead((Player)sender, 5);
+
+		}
 		return false;
 	}
 
+	public Material setType(String materialName)
+	{
+
+		Material m = null;
+		if (materialName.equalsIgnoreCase("PLANKS")) { m = Material.WOOD;
+		} else {
+			if (materialName.equalsIgnoreCase("WOOL")) { m = Material.WOOL;
+			} else {
+				if (materialName.equalsIgnoreCase("GLASS")) { m = Material.GLASS;
+				} else {
+					if (materialName.equalsIgnoreCase("BOOKSHELF")) { m = Material.BOOKSHELF;
+					}
+
+				}
+			}
+		}
+		return m;
+	}
 }
--- src/net/minedev/listeners/HgPlayerListener.java
+++ src/net/minedev/listeners/HgPlayerListener.java
@@ -5,7 +5,11 @@ import org.bukkit.event.player.PlayerListener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.util.Vector;
 
+import couk.Adamki11s.Extras.Events.ExtrasEvents;
+
 public class HgPlayerListener extends PlayerListener{
+
+	ExtrasEvents extrasEvts = new ExtrasEvents();
 
 	public void onPlayerMove(PlayerMoveEvent event)
 	{
@@ -16,6 +20,7 @@ public class HgPlayerListener extends PlayerListener{
 			Vector dir = p.getLocation().getDirection().multiply(((3)*(0.3))/2).setY(0.1);
 			p.setVelocity(dir);
 		}
+
 	}
 
 }
