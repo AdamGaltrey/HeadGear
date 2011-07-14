d7bbc16934dbbb7aed83ed6cad0e9126caab6c16
iPhysX <jasonwalsh@blueyonder.co.uk>
2011-07-14 01:27:35 -0700

    finished the initial list command!

--- src/net/minedev/commands/ListCommand.java
+++ src/net/minedev/commands/ListCommand.java
@@ -7,20 +7,29 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class ListCommand implements CommandExecutor {
+	
+	ChatColor a = ChatColor.AQUA;
+	ChatColor b = ChatColor.BLUE;
+	ChatColor g = ChatColor.GREEN;
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label,
 			String[] args) {
+		Player p = (Player)sender;
 		if (cmd.getName().equalsIgnoreCase("headgear") || cmd.getName().equalsIgnoreCase("hg"))
 		{
 			if (args.length == 1)
 			{
 				if (args[0].equalsIgnoreCase("list"))
 				{
-					Player p = (Player)sender;
-					ChatColor a = ChatColor.AQUA;
-					p.sendMessage(a + "");
-					p.sendMessage(a + "");
+					
+					p.sendMessage(g + "         ---  HeadGear List  ---");
+ 					p.sendMessage(a + "Invincible " + b + "Makes you temporarily Invincible!");
+					p.sendMessage(a + "Glow " + b + "Makes you glow in the dark for a short time!");
+					p.sendMessage(a + "Invisible " + b + "Makes you invisible to all except admins!");
+					p.sendMessage(a + "Superspeed " + b + "Makes you travel at superspeed for a while!");
+					p.sendMessage(g + "Type /<name> info to find out more about it!");
+						
 				}
 			}
 		}
