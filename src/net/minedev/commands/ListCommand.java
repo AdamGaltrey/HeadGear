912269db39901c7bebdf9fbfc93e511585a7b6ed
iPhysX <jasonwalsh@blueyonder.co.uk>
2011-07-14 01:18:58 -0700

    LOL chatcolor.

--- src/net/minedev/commands/ListCommand.java
+++ src/net/minedev/commands/ListCommand.java
@@ -1,15 +1,29 @@
 package net.minedev.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class ListCommand implements CommandExecutor {
 
 	@Override
-	public boolean onCommand(CommandSender arg0, Command arg1, String arg2,
-			String[] arg3) {
-		// TODO Auto-generated method stub
+	public boolean onCommand(CommandSender sender, Command cmd, String label,
+			String[] args) {
+		if (cmd.getName().equalsIgnoreCase("headgear") || cmd.getName().equalsIgnoreCase("hg"))
+		{
+			if (args.length == 1)
+			{
+				if (args[0].equalsIgnoreCase("list"))
+				{
+					Player p = (Player)sender;
+					ChatColor a = ChatColor.AQUA;
+					p.sendMessage(a + "");
+					p.sendMessage(a + "");
+				}
+			}
+		}
 		return false;
 	}
 
